894d356f792c8b38f4c525294ce6aac614139f41
niko994 <nicolasdominguez72@gmail.com>
2023-03-19 17:59:14 -0300

    complete game (only console.log)

--- script.js
+++ script.js
@@ -1,16 +1,81 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+/* generate a random number between 0 and 3 (max) */
+/* Assigned a label to each number */
 
 function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
     if (choice < 1) {
-        return "Rock"
+        return "rock"
     } else if (choice  > 1) {
-        return "Paper"
+        return "paper"
     } else {
         return "scissors"
     }
 }
 
-console.log(getComputerChoice())
+function getPlayerChoice () {
+    let playerSelection = prompt('Do you choose Rock, Paper or Scissors?').toLowerCase()
+          if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+              console.log("You chose " + playerSelection);
+              return playerSelection
+          } else {
+              console.log('Error! Pick Rock, Paper or Scissors')
+              return getPlayerChoice()
+          }
+  }
+
+function playRound(playerSelection,computerChoice) {
+    if (playerSelection === computerChoice) {
+        return "it's a draw" 
+    } else if (playerSelection == "scissors" && computerChoice == "rock" ) {
+        return "You lose"
+    } else if (playerSelection == "scissors" && computerChoice == "paper") {
+        return "You Win"
+    } else if (playerSelection == "rock" && computerChoice == "scissors") {
+        return "You Win"
+    } else if (playerSelection == "rock" && computerChoice == "paper") {
+        return "You lose"
+    } else if (playerSelection == "paper" && computerChoice == "scissors" ) {
+        return "You lose"
+    } else if (playerSelection == "paper" && computerChoice == "rock") {
+        return "You Win"
+    } else {
+        return "Thats not an option"
+    }
+}
+
+function game() {
+    for (let i = 1; i < 6; i++) {
+        console.log("Round" + i)
+        let playerSelection = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        console.log("computer chose " + computerChoice)
+        console.log(playRound(playerSelection,computerChoice))
+        if (playRound(playerSelection,computerChoice) == "You Win") {
+            userCounter++
+            console.log("User score : " + userCounter + " Computer score : "  + computerCounter)
+        } else if (playRound(playerSelection,computerChoice) == "You lose") {
+            computerCounter++
+            console.log("User score : " + userCounter + " Computer score : " + computerCounter)
+        } else if (playRound(playerSelection,computerChoice) == "it's a draw") {
+            console.log("User score : " + userCounter + " Computer score : " + computerCounter)
+        }
+    }
+}
+
+function gameResult () {
+    if (userCounter > computerCounter && i == 5) {
+        console.log("You win the game")
+    } else if (userCounter == computerCounter) { 
+        console.log("it's a draw")
+    } else {
+        console.log("You lose the game")
+    }
+}
+
+let computerChoice = getComputerChoice ();
+let userCounter = 0
+let computerCounter = 0
+let i = 1
+
+game()
+console.log(gameResult())
